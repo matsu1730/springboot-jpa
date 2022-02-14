1a7077d5d5511e1d9b249a0fdd12f442906db042
matsu1730 <alf.matsuoka2000@gmail.com>
2022-02-14 17:58:19 -0300

    Service layer, component registration

---
+++ src/main/java/com/amatsuoka/jpa/services/UserService.java
@@ -0,0 +1,26 @@
+package com.amatsuoka.jpa.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.amatsuoka.jpa.entities.User;
+import com.amatsuoka.jpa.repositories.UserRepository;
+
+@Service
+public class UserService {
+
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll() {
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+}
