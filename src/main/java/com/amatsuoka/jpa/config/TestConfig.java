8049f000acc1c6fd5e02ebefb2dfa5a064fdafde
matsu1730 <alf.matsuoka2000@gmail.com>
2022-02-15 20:35:25 -0300

    Payment, one-to-one association

--- src/main/java/com/amatsuoka/jpa/config/TestConfig.java
+++ src/main/java/com/amatsuoka/jpa/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.amatsuoka.jpa.entities.Category;
 import com.amatsuoka.jpa.entities.Order;
 import com.amatsuoka.jpa.entities.OrderItem;
+import com.amatsuoka.jpa.entities.Payment;
 import com.amatsuoka.jpa.entities.Product;
 import com.amatsuoka.jpa.entities.User;
 import com.amatsuoka.jpa.entities.enums.OrderStatus;
@@ -80,6 +81,11 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 
 	}
 
